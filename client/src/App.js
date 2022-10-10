c4648881325a39b098d081c6badadc190c479708
Sami Warraich <samiwarraich51@gmail.com>
2022-10-10 23:35:58 +0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <CreatePost />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+};
+
+export default App;
